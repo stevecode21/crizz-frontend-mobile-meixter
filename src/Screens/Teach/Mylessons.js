3bccabdaff831c6abe5f551dfd375da566db3d8f
stevecode21 <suarezsteve21@gmail.com>
2021-01-28 23:12:18 -0500

    Removing console.log()

--- src/Screens/Teach/Mylessons.js
+++ src/Screens/Teach/Mylessons.js
@@ -189,7 +189,6 @@ export default function Mylessons() {
 		catch (error) 
 		{
 			setLoading(false)
-			console.log(error)
 	        if (error.status == 401) 
 	        {
 	          showErrorToast(localeProvider.name == 'en' ? error.message_en : error.message_es)
